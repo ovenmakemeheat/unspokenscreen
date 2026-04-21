65cb7ca5d10b0aeee4d0ffd17efce9618923f320
microhum <Guntee12123@gmail.com>
2026-04-21 09:55:40 +0700

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@unspokenscreen/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
