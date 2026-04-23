01fadb962ef605dcf6a7164310b27434bf712fd9
microhum <Guntee12123@gmail.com>
2026-04-23 16:55:20 +0700

    fix: try to change env prefix

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -5,12 +5,17 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1),
-    SUPABASE_URL: z.url(),
-    SUPABASE_ANON_KEY: z.string().min(1),
+    // SUPABASE_URL: z.url(),
+    // SUPABASE_ANON_KEY: z.string().min(1),
     CORS_ORIGIN: z.url(),
     NODE_ENV: z
       .enum(["development", "production", "test"])
       .default("development"),
+  },
+  clientPrefix: "PUBLIC_",
+  client: {
+    PUBLIC_SUPABASE_URL: z.url(),
+    PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
